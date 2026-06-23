9bde709b6c0f1da49d16221c76160061518462c1
rachidedndev <lucas_rachide92@hotmail.com>
2026-06-23 10:17:57 -0300

    fix: adicionando configuração de storages para o django 6

--- backend/settings.py
+++ backend/settings.py
@@ -108,6 +108,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
 
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Configuração obrigatória para o Django 6.0+ usar o WhiteNoise em produção
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
